efe452fd2c2d575abeca7a9ae59d3fd36936aa75
rahlen1337 <h22mafr@axevalla.fhsk.se>
2024-01-24 10:41:08 +0100

    First!

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using SadConsole.Configuration;
+
+Settings.WindowTitle = "My SadConsole Game";
+
+Game.Create(90, 30, Startup);
+Game.Instance.Run();
+Game.Instance.Dispose();
+
+static void Startup(object? sender, GameHost host)
+{
+    Game.Instance.StartingConsole!.FillWithRandomGarbage(SadConsole.Game.Instance.StartingConsole!.Font);
+    Game.Instance.StartingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, Mirror.None);
+    Game.Instance.StartingConsole.Print(4, 4, "Hello from SadConsole");
+}
