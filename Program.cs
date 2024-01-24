52a88bcb1fb0bf510047cfe731ac3222fcedc38f
rahlen1337 <h22mafr@axevalla.fhsk.se>
2024-01-24 11:17:36 +0100

    THird

--- Program.cs
+++ Program.cs
@@ -13,5 +13,8 @@ static void Startup(object? sender, GameHost host)
     Game.Instance.StartingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, Mirror.None);
     Game.Instance.StartingConsole.Print(4, 4, "Hello from SadConsole");
     Game.Instance.StartingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateBorder(new ColoredGlyph(Color.Violet, Color.Black, 176)));
-
+    Game.Instance.StartingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateStyledBox(ICellSurface.ConnectedLineThin,
+                                                    new ColoredGlyph(Color.Violet, Color.Black)));
+    Game.Instance.StartingConsole.DrawCircle(new Rectangle(5, 8, 16, 8), ShapeParameters.CreateFilled(new ColoredGlyph(Color.Violet, Color.Black, 176),
+                                                       new ColoredGlyph(Color.White, Color.Black)));
 }
