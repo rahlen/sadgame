ba2cc556e8e0272919c97dd7f4fbda3c8856788f
rahlen1337 <h22mafr@axevalla.fhsk.se>
2024-01-24 11:28:19 +0100

    Forth

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ Game.Instance.Dispose();
 static void Startup(object? sender, GameHost host)
 {
     
-    Game.Instance.StartingConsole!.FillWithRandomGarbage(SadConsole.Game.Instance.StartingConsole!.Font);
+  
     Game.Instance.StartingConsole.Fill(new Rectangle(3, 3, 23, 3), Color.Violet, Color.Black, 0, Mirror.None);
     Game.Instance.StartingConsole.Print(4, 4, "Hello from SadConsole");
     Game.Instance.StartingConsole.DrawBox(new Rectangle(3, 3, 23, 3), ShapeParameters.CreateBorder(new ColoredGlyph(Color.Violet, Color.Black, 176)));
@@ -17,4 +17,5 @@ static void Startup(object? sender, GameHost host)
                                                     new ColoredGlyph(Color.Violet, Color.Black)));
     Game.Instance.StartingConsole.DrawCircle(new Rectangle(5, 8, 16, 8), ShapeParameters.CreateFilled(new ColoredGlyph(Color.Violet, Color.Black, 176),
                                                        new ColoredGlyph(Color.White, Color.Black)));
+    Game.Instance.StartingConsole.DrawLine(new Point(60, 5), new Point(66, 20), '$', Color.AnsiBlue, Color.AnsiBlueBright, Mirror.None);
 }
